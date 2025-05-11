5e00a27c82003789185f5afea0dbd848d222d836
ariadna-gh <ariadna.yakovlev@krfk.kai.edu.ua>
2025-05-11 20:27:43 +0300

    Add files via upload

---
+++ oop3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace lab3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+        }
+
+
+
+        /*1)У наведеному коді є порушення правил неймінгу та CodeStyle.
+        Виправіть всі помилки згідно з CamelCase та PascalCase.*/
+        private static void Task1()
+        {
+            int userAge = 20;
+            string userName = "Андрій";
+            Console.WriteLine("Привіт, " + userName + "! Твій вік: " + userAge);
+        }
+
+
+        //2) Уявіть, що у вас є список змінних. Але деякі назви неправильні, тому що вони використовують зарезервовані слова C#.
+        //Виправіть код так, щоб він компілювався без помилок.
+        private static void Task2()
+        {
+            int userAge = 20;
+            string userName = "Андрій";
+            Console.WriteLine("Привіт, " + userName + "! Твій вік: " + userAge);
+        }
+
+
+        //3) Є код без коментарів. Додайте пояснювальні коментарі до кожного рядка.*/
+
+        private static void Task3()
+        {
+            string name = "Анна"; // змінна для збереження імені
+            int age = 25; // змінна для збереження віку
+            Console.WriteLine("Привіт, " + name + "! Твій вік: " + age); // вивід привітання на екран
+        }
+    }
+}
