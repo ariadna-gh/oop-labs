dc6a559ff4fb5c1dfb4e5463692c0d39940d58de
ariadna-gh <ariadna.yakovlev@krfk.kai.edu.ua>
2025-06-04 22:58:41 +0300

    Add files via upload

---
+++ oop7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace oop7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 2; i <= 20; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
